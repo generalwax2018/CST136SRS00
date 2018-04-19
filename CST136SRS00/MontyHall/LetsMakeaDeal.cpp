e685b49364389b1467419cf84fd10781e3a94225
generalwax2018 <generalwax2018@users.noreply.github.com>
2018-04-18 19:48:48 -0700

    test one
    test one

---
+++ CST136SRS00/MontyHall/LetsMakeaDeal.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "LetsMakeaDeal.h"
+#include <random>
+#include <vector>
+
+
+using namespace std;
+
+
+LetsMakeaDeal::LetsMakeaDeal(int const doors, int const reveal_doors, string const decision) :
+	dTotal{ doors }, dRevealed(reveal_doors)
+{
+	
+	dec_collection_doors_.assign(doors, );
+	st_collection_doors_.(doors, ); 
+		runGame(); 
+
+
+}
+
+void LetsMakeaDeal::runGame()
+{
+	
+	
+
+	//std::vector<bool> doors = { };
+
+}
+
+
+//To avoid bugs and pass around program arguments - c++ programming language pg 245
+
+
+int LetsMakeaDeal::makeDoors(int a)
+{
+	
+	return 0; 
+}
+
+
+
+
+int LetsMakeaDeal::makeRandom(int const max)
+{
+	static random_device random; 
+	static mt19937 sGenertor {42};
+	uniform_int_distribution<int> const dis(0, (max -1));
+
+	return dis(sGenertor);
+
+}
---
+++ CST136SRS00/MontyHall/LetsMakeaDeal.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "stdafx.h"
+#include <string>
+#include <vector>
+
+using namespace std; 
+
+class LetsMakeaDeal
+{
+public:
+	
+	enum class gameresult { won, lost };
+	
+	LetsMakeaDeal(int const doors, int const reveal_doors, string const decision, string const prize);
+	void runGame();
+	
+	
+private:
+	enum class prize {goat, car};
+	enum class decision {stay, switch_door};
+	enum class doorState{open, close};
+
+	int const dTotal;
+	int const dRevealed;
+
+	
+	string const dPrize;
+	string const dDecision; 
+	string const dState;
+	 
+	vector<prize> dec_collection_doors_;
+	vector<doorState> st_collection_doors_; 
+
+	int makeDoors(int a);
+	int makeRandom(int const max);
+};
+
